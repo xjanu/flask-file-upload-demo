dd6d9a8b48a122cad385957ffd4a05a0c453a0b2
Martin Janů <xjanu@fi.muni.cz>
2024-12-27 00:10:44 +0100

    Show all uploaded files

--- app.py
+++ app.py
@@ -4,21 +4,30 @@ from flask import Flask, request, render_template
 import os
 
 UPLOAD_FOLDER = 'static'
+APP_TITLE = "Flask file upload demo"
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.get('/')
-def endpoint_get():
-    return render_template('index.html', title="Flask file upload demo")
-
-# file upload endpoint
-@app.post('/')
+@app.route('/', methods=["GET", "POST"])
 def endpoint_post():
 
-    file = request.files['upload']
+    # Save the uploaded file (POST)
+    uploaded = None
+    if request.files:
+        file = request.files['upload']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
+        uploaded = file.filename
 
-    path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(path)
+    # List all uploaded files
+    all_files = os.listdir(UPLOAD_FOLDER)
+    all_files.sort()
+    print(all_files)
 
-    return render_template('index.html', title="Flask file upload demo", content=f"<a href='{path}'>{file.filename}</a>")
+    return render_template(
+        'index.html',
+        title=APP_TITLE,
+        uploaded=uploaded,
+        all_files=all_files
+    )
